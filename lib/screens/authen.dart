d120f13c80408ec0bdad81583ea28c4107bc42dd
masterUNG <phrombutr@gmail.com>
2020-05-07 15:49:59 +0700

    update

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ungdaily/utility/my_style.dart';
+import 'package:ungdaily/utility/normal_dialog.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -7,22 +9,58 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  // Field
+  String user, password;
+
+  Future<void> checkAuthen() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth
+        .signInWithEmailAndPassword(email: user, password: password)
+        .then((value) => print('Authen Success'))
+        .catchError((value) {
+          String title = value.code;
+          String message = value.message;
+          normalDialog(context, title, message);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.navigate_next, size: 36.0,),
-        onPressed: () {},
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.navigate_next,
+          size: 36.0,
+        ),
+        onPressed: () {
+          if (user == null ||
+              user.isEmpty ||
+              password == null ||
+              password.isEmpty) {
+            normalDialog(context, 'Have Space', 'Please Fill Every Blank');
+          } else {
+            checkAuthen();
+          }
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            MyStyle().mySizeBox(),
-            userForm(),
-            MyStyle().mySizeBox(),
-            passwordForm(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: <Color>[Colors.white, Colors.green.shade900],
+            radius: 1.0,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              MyStyle().mySizeBox(),
+              userForm(),
+              MyStyle().mySizeBox(),
+              passwordForm(),
+            ],
+          ),
         ),
       ),
     );
@@ -31,6 +69,8 @@ class _AuthenState extends State<Authen> {
   Widget userForm() => Container(
         width: 250.0,
         child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          onChanged: (value) => user = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.account_box),
             labelText: 'User :',
@@ -42,6 +82,8 @@ class _AuthenState extends State<Authen> {
   Widget passwordForm() => Container(
         width: 250.0,
         child: TextField(
+          obscureText: true,
+          onChanged: (value) => password = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.lock),
             labelText: 'Password :',
